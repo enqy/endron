9b838db56543070abb6d78ca1a545f540e83e668
Woze Parrrot <wozeparrot@gmail.com>
2020-10-06 15:25:59 -0400

    analysis and small syntax change

--- src/analysis.zig
+++ src/analysis.zig
@@ -1,1 +1,22 @@
-pub const Checker = struct {};
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const ast = @import("ast.zig");
+const Tree = ast.Tree;
+const Node = ast.Node;
+
+const Pass = fn (*Allocator, *Tree, *State) anyerror!void;
+const Passes = [_]Pass{};
+
+pub fn analyze(tree: *Tree) !void {
+    var arena = std.heap.ArenaAllocator.init(tree.gpa);
+    defer arena.deinit();
+
+    var state = State{};
+
+    for (Passes) |pass| {
+        try pass(&arena.allocator, tree, &state);
+    }
+}
+
+pub const State = struct {};
