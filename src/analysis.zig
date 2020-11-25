b5ca8272200ff945a06239f3b47374a6ec55e355
Woze Parrrot <wozeparrot@gmail.com>
2020-11-25 18:59:59 -0500

    tree to typed tree

--- src/analysis.zig
+++ src/analysis.zig
@@ -5,7 +5,10 @@ const ast = @import("ast.zig");
 const Tree = ast.Tree;
 const Node = ast.Node;
 
-const Pass = fn (*Allocator, *const Tree, *State) anyerror!void;
+const tast = @import("typed_ast.zig");
+const TypedTree = tast.TypedTree;
+
+const Pass = fn (*Allocator, *const Tree, *TypedTree) anyerror!void;
 const Passes = [_]Pass{
     @import("analysis/0_typing.zig").pass,
 };
@@ -14,94 +17,9 @@ pub fn analyze(tree: *const Tree) !void {
     var arena = std.heap.ArenaAllocator.init(tree.gpa);
     defer arena.deinit();
 
-    var state = try State.init(tree.gpa);
-    defer state.deinit();
+    var ttree = try TypedTree.transform(&arena.allocator, tree);
 
     for (Passes) |pass| {
-        try pass(&arena.allocator, tree, &state);
+        try pass(&arena.allocator, tree, &ttree);
     }
 }
-
-pub const State = struct {
-    pub const Type = struct {
-        pub const Kind = enum {
-            void,
-            u8,
-            u16,
-            u32,
-            u64,
-            i8,
-            i16,
-            i32,
-            i64,
-            f32,
-            f64,
-            bool,
-        };
-        kind: Kind,
-    };
-    pub const TypeId = usize;
-
-    types: std.ArrayList(Type),
-    type_map: std.StringHashMap(TypeId),
-
-    pub fn init(gpa: *Allocator) !State {
-        var state = State{
-            .types = std.ArrayList(Type).init(gpa),
-            .type_map = std.StringHashMap(TypeId).init(gpa),
-        };
-
-        try state.initBuiltinTypes();
-
-        return state;
-    }
-
-    pub fn deinit(state: *State) void {
-        state.types.deinit();
-        state.type_map.deinit();
-    }
-
-    pub fn initBuiltinTypes(state: *State) !void {
-        try state.registerType("void", .{
-            .kind = .void,
-        });
-        try state.registerType("u8", .{
-            .kind = .u8,
-        });
-        try state.registerType("u16", .{
-            .kind = .u16,
-        });
-        try state.registerType("u32", .{
-            .kind = .u32,
-        });
-        try state.registerType("u64", .{
-            .kind = .u64,
-        });
-        try state.registerType("i8", .{
-            .kind = .i8,
-        });
-        try state.registerType("i16", .{
-            .kind = .i16,
-        });
-        try state.registerType("i32", .{
-            .kind = .i32,
-        });
-        try state.registerType("i64", .{
-            .kind = .i64,
-        });
-        try state.registerType("f32", .{
-            .kind = .f32,
-        });
-        try state.registerType("f64", .{
-            .kind = .f64,
-        });
-        try state.registerType("bool", .{
-            .kind = .bool,
-        });
-    }
-
-    fn registerType(state: *State, name: []const u8, t: Type) !void {
-        try state.types.append(t);
-        try state.type_map.put(name, state.types.items.len - 1);
-    }
-};
--- src/analysis/0_typing.zig
+++ src/analysis/0_typing.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const State = @import("../analysis.zig").State;
 const Tree = @import("../ast.zig").Tree;
+const TypedTree = @import("../typed_ast.zig").TypedTree;
 
-pub fn pass(arena: *Allocator, tree: *const Tree, state: *State) !void {
-
-}
+pub fn pass(arena: *Allocator, tree: *const Tree, ttree: *TypedTree) !void {}
