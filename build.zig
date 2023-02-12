f9ee6bc5c5107ba82f1ff518604c19ef485bf0a0
Woze Parrot <wozeparrot@gmail.com>
2023-02-12 01:58:33 -0500

    feat: update build.zig to latest zig

--- build.zig
+++ build.zig
@@ -1,19 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
-
-    const exe = b.addExecutable("endron", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const exe = b.addExecutable(.{
+        .name = "endron",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.linkLibC();
     exe.install();
 
@@ -26,10 +22,16 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const tests = b.addTest("test/test.zig");
-    tests.setTarget(target);
-    tests.setBuildMode(mode);
-    tests.addPackagePath("endron", "src/lib.zig");
+    b.addModule(.{
+        .name = "endron",
+        .source_file = .{ .path = "src/lib.zig" },
+    });
+
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "test/test.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
