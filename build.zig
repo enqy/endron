1c20a3a90155ecd67ac8ddd610ae0f56c975b76e
Woze Parrot <wozeparrot@gmail.com>
2023-04-01 02:21:40 -0400

    feat: fix test running

--- build.zig
+++ build.zig
@@ -22,8 +22,7 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    b.addModule(.{
-        .name = "endron",
+    const endron_mod = b.addModule("endron", .{
         .source_file = .{ .path = "src/lib.zig" },
     });
 
@@ -32,7 +31,9 @@ pub fn build(b: *std.build.Builder) void {
         .target = target,
         .optimize = optimize,
     });
+    tests.addModule("endron", endron_mod);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
+    test_step.dependOn(&tests.run().step);
 }
